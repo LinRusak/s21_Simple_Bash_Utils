142490d4e64a4d52d6197670755046850cf0722c
Blanca Sallor <blancasa@pr-c10.nsk.21-school.ru>
2023-11-30 12:16:57 +0700

    for leaks test

--- src/cat/test_cat/test_func_cat.sh
+++ src/cat/test_cat/test_func_cat.sh
@@ -9,41 +9,41 @@ declare -a tests =
 "VAR test_case_cat.txt"
 
 declare -a extra=(
-"-b test_1_cat.txt"
-"-e -v test_1_cat.txt"
-"-n test_1_cat.txt"
-"-s test_1_cat.txt"
-"-t -v test_1_cat.txt"
-"-e -v test_2_cat.txt"
-"-n test_2_cat.txt"
-"-t -v test_2_cat.txt"
-"-b test_3_cat.txt"
-"-e -v test_3_cat.txt"
-"-n test_3_cat.txt"
-"-t -v test_3_cat.txt"
-"-s test_3_cat.txt"
-"-b test_4_cat.txt"
-"-e -v test_4_cat.txt"
-"-n test_4_cat.txt"
-"-s test_4_cat.txt"
-"-t -v test_4_cat.txt"
-"-t -v test_5_cat.txt"
-"-v test_5_cat.txt"
-"-e -v test.txt"
-"-t -v test.txt"
-"-n test.txt"
-"-b no_file.txt"
-"-e -v no_file.txt"
-"-n no_file.txt"
-"-s no_file.txt"
-"-t -v no_file.txt"
-"-b test_case_cat.txt"
-"-e -v test_case_cat.txt"
-"-n test_case_cat.txt"
-"-s test_case_cat.txt"
-"-t -v test_case_cat.txt"
-"-v test_case_cat.txt"
-"-n test_1_cat.txt test_2_cat.txt"
+"-b ./test_cat/test_1_cat.txt"
+"-e -v ./test_cat/test_1_cat.txt"
+"-n ./test_cat/test_1_cat.txt"
+"-s ./test_cat/test_1_cat.txt"
+"-t -v ./test_cat/test_1_cat.txt"
+"-e -v ./test_cat/test_2_cat.txt"
+"-n ./test_cat/test_2_cat.txt"
+"-t -v ./test_cat/test_2_cat.txt"
+"-b ./test_cat/test_3_cat.txt"
+"-e -v ./test_cat/test_3_cat.txt"
+"-n ./test_cat/test_3_cat.txt"
+"-t -v ./test_cat/test_3_cat.txt"
+"-s ./test_cat/test_3_cat.txt"
+"-b ./test_cat/test_4_cat.txt"
+"-e -v ./test_cat/test_4_cat.txt"
+"-n ./test_cat/test_4_cat.txt"
+"-s ./test_cat/test_4_cat.txt"
+"-t -v ./test_cat/test_4_cat.txt"
+"-t -v ./test_cat/test_5_cat.txt"
+"-v ./test_cat/test_5_cat.txt"
+"-e -v ./test_cat/test.txt"
+"-t -v ./test_cat/test.txt"
+"-n ./test_cat/test.txt"
+"-b ./test_cat/no_file.txt"
+"-e -v ./test_cat/no_file.txt"
+"-n ./test_cat/no_file.txt"
+"-s ./test_cat/no_file.txt"
+"-t -v ./test_cat/no_file.txt"
+"-b ./test_cat/test_case_cat.txt"
+"-e -v ./test_cat/test_case_cat.txt"
+"-n ./test_cat/test_case_cat.txt"
+"-s ./test_cat/test_case_cat.txt"
+"-t -v ./test_cat/test_case_cat.txt"
+"-v ./test_cat/test_case_cat.txt"
+"-n ./test_cat/test_1_cat.txt ./test_cat/test_2_cat.txt"
 )
 
 testing()
@@ -81,64 +81,64 @@ do
     done
 done
 
-# # 2 параметра
-# for var1 in b e n s t v
-# do
-#     for var2 in b e n s t v
-#     do
-#         if [ $var1 != $var2 ]
-#         then
-#             for i in "${tests[@]}"
-#             do
-#                 var="-$var1 -$var2"
-#                 testing $i
-#             done
-#         fi
-#     done
-# done
+# 2 параметра
+for var1 in b e n s t v
+do
+    for var2 in b e n s t v
+    do
+        if [ $var1 != $var2 ]
+        then
+            for i in "${tests[@]}"
+            do
+                var="-$var1 -$var2"
+                testing $i
+            done
+        fi
+    done
+done
 
-# # 3 параметра
-# for var1 in b e n s t v
-# do
-#     for var2 in b e n s t v
-#     do
-#         for var3 in b e n s t v
-#         do
-#             if [ $var1 != $var2 ] && [ $var2 != $var3 ] && [ $var1 != $var3 ]
-#             then
-#                 for i in "${tests[@]}"
-#                 do
-#                     var="-$var1 -$var2 -$var3"
-#                     testing $i
-#                 done
-#             fi
-#         done
-#     done
-# done
+# 3 параметра
+for var1 in b e n s t v
+do
+    for var2 in b e n s t v
+    do
+        for var3 in b e n s t v
+        do
+            if [ $var1 != $var2 ] && [ $var2 != $var3 ] && [ $var1 != $var3 ]
+            then
+                for i in "${tests[@]}"
+                do
+                    var="-$var1 -$var2 -$var3"
+                    testing $i
+                done
+            fi
+        done
+    done
+done
 
-# # 4 параметра
-# for var1 in b e n s t v
-# do
-#     for var2 in b e n s t v
-#     do
-#         for var3 in b e n s t v
-#         do
-#             for var4 in b e n s t v
-#             do
-#                 if [ $var1 != $var2 ] && [ $var2 != $var3 ] \
-#                 && [ $var1 != $var3 ] && [ $var1 != $var4 ] \
-#                 && [ $var2 != $var4 ] && [ $var3 != $var4 ]
-#                 then
-#                     for i in "${tests[@]}"
-#                     do
-#                         var="-$var1 -$var2 -$var3 -$var4"
-#                         testing $i
-#                     done
-#                 fi
-#             done
-#         done
-#     done
-# done
+# 4 параметра
+for var1 in b e n s t v
+do
+    for var2 in b e n s t v
+    do
+        for var3 in b e n s t v
+        do
+            for var4 in b e n s t v
+            do
+                if [ $var1 != $var2 ] && [ $var2 != $var3 ] \
+                && [ $var1 != $var3 ] && [ $var1 != $var4 ] \
+                && [ $var2 != $var4 ] && [ $var3 != $var4 ]
+                then
+                    for i in "${tests[@]}"
+                    do
+                        var="-$var1 -$var2 -$var3 -$var4"
+                        testing $i
+                    done
+                fi
+            done
+        done
+    done
+done
 
 echo "\033[31mFAIL: $FAIL\033[0m"
 echo "\033[32mSUCCESS: $SUCCESS\033[0m"
